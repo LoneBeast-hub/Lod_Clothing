bc1f33d3ab03bce516a729ebc8b872f38f431249
LOD <yusufoyindamola5@gmail.com>
2022-03-17 21:05:52 +0100

    update our menu-items to navigate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
 import { HomePage } from './pages/homepage/homepage.component';
+
+const HatsPage = () => {
+  return(
+    <div>
+      <h1>HATS</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-      {/* Use Homepage component */}
-      <HomePage/>
+      <Routes>
+        <Route exact path='/' element={ <HomePage/> } />
+        <Route exact path='/hats' element={ <HatsPage/> } />
+      </Routes>
     </div>
   );
 }
